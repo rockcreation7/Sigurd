14ded1a75b24e077628700508c805c7e7ddc4559
rock <rockcreation7@gmail.com>
2022-07-07 15:23:32 +0100

    update new

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-pub struct ThreadPool;
+
+use std::thread;
+
+pub struct ThreadPool{
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
-    }
+  
 
     pub fn execute<F>(&self, f: F)
     where
@@ -11,5 +14,24 @@ impl ThreadPool {
         // We need Send to transfer the closure from one thread to another and 'static because we don’t know how long the thread will take to execute. 
         F: FnOnce() + Send + 'static,
     {
+    }
+
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            // create some threads and store them in the vector
+        }
+
+        ThreadPool { threads }
     }
 }
