d82fc7fa3aabc8d3007f20568531ce0b6dbc3eac
rock <rockcreation7@gmail.com>
2022-08-16 21:59:49 +0100

    add arc

--- cashier/src/main.rs
+++ cashier/src/main.rs
@@ -1,79 +1,86 @@
-use actix_web::{web, post, App, HttpServer, Responder}; 
-use awc::Client; // https://crates.io/crates/awc //https://github.com/actix/examples/blob/master/https-tls/awc-https/src/main.rs
+use actix_web::{post, web::{self, Data}, App, HttpServer, Responder, HttpRequest};
+// https://crates.io/crates/awc //https://github.com/actix/examples/blob/master/https-tls/awc-https/src/main.rs
+use derive_more::{Display, Error};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::Mutex;
+
 mod key;
+
+
 /* use crate::game::*; */
-#[actix_web::main] 
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(calculate_ticket)
-        
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let data = Data::new(Mutex::new(key::get_product()));
+
+    // let product_data:HashMap<i32, key::Product> = key::get_product();
+    HttpServer::new(move || App::new().app_data(Data::clone(&data)).service(calculate_ticket))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct TicketCalResult {
-    price: f32,
-    is_toy: bool,
-    ticket: f32,
-    ticketdiscount: f32,
+struct OrderCalResult {
+    price: f32, 
 }
 
 #[derive(Debug, Deserialize, Clone)]
-struct TicketCalData {
-    price: f32,
-    is_toy: bool,
+struct Order {
+    // price: f32,
+    // is_toy: bool,
     order: Vec<Item>,
 }
 
 #[derive(Debug, Deserialize, Clone)]
 struct Item {
-    id: f32,
+    id: i32,
     qty: i32,
 }
 
+#[derive(Deserialize)]
+struct StatusData {
+    // #[serde(rename = "status")]
+    // status: i32,
+    ok: bool,
+}
+
 #[post("/calculate_order")]
-async fn calculate_ticket(data: web::Json<TicketCalData>) -> impl Responder {
+async fn calculate_ticket(req: HttpRequest, data: web::Json<Order>) -> impl Responder {
     // make request here
-    let client = Client::default();
-
-    let res = client
-        .get("http://www.rust-lang.org")    // <- Create request builder
-        .insert_header(("User-Agent", "Actix-web"))
-        .send()                             // <- Send http request
-        .await;
-
-    println!("Response: {:?}", res);     
-    // make request here
-  
     println!("model: {:?}", &data);
-    println!("{} {}", key::BOT_KEY, key::CHAT_ID);
-    let ticket: f32;
-
-    let ticketforprice: f32 = data.price / 10.0;
-    let ticketforplay: f32 = ticketforprice * 5.0 / 10.0;
-    ticket = ticketforprice + ticketforplay;
-
+    let product_data = req.app_data::<Data<Mutex<HashMap<i32, key::Product>>>>().unwrap().lock().unwrap();
+    let mut order_total = 0.00;
     for elem in data.order.clone() {
         println!("{} {}", elem.id, elem.qty);
+        let price_of_item = product_data[&elem.id].price;
+        order_total = order_total + price_of_item * elem.qty as f32;
     }
 
-    println!("{:#?}", ticket);
+    let telegram_api = "https://api.telegram.org/bot".to_owned()
+        + key::BOT_KEY
+        + "/sendMessage?chat_id="
+        + key::CHAT_ID
+        + "&parse_mode=Markdown&text=hello";
 
-    let mut ticketdiscount: f32 = 0.0;
+    let resp = reqwest::get(telegram_api).await;
+    // println!("Response: {:?}", resp);
 
-    if data.is_toy && ((data.price / 100.0) > 1.0) {
-        ticketdiscount = (data.price / 100.0).floor()
+    match resp {
+        Ok(data) => {
+            println!("{:#?}", data);
+            let json_data = data.json::<StatusData>().await.unwrap();
+            println!("Response: {:?}", json_data.ok);
+            /*   return web::Json(OrderCalResult {
+                price: 0.0
+            }); */
+        }
+        Err(err) => {
+            println!("{:#?}", err);
+        }
     }
 
-    web::Json(TicketCalResult {
-        price: 0.0,
-        is_toy: true,
-        ticket: ticket.round(),
-        ticketdiscount: ticketdiscount,
+    web::Json(OrderCalResult {
+        price: order_total,
     })
 }
